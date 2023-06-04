6c578e908f39c0f7c05e6e71611c48e74eef1ddc
md-abir-alahe <mdabiralahe@gmail.com>
2023-06-04 13:47:19 +0600

    Dynamic page title by Route Using React Helmet

---
+++ src/pages/Menu/Menu.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Helmet } from 'react-helmet-async';
+
+const Menu = () => {
+    return (
+        <div>
+            <Helmet>
+                <title>Bistro Boss | Menu</title>
+            </Helmet>
+            <h3>Menu</h3>
+        </div>
+    );
+};
+
+export default Menu;
