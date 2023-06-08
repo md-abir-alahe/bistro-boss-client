228b527be437f002d69c15684fcb41d953f31243
md-abir-alahe <mdabiralahe@gmail.com>
2023-06-08 23:10:31 +0600

    setup login page and auth context

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,44 +1,11 @@
-import React, { useContext, useEffect, useRef, useState } from 'react';
-import { loadCaptchaEnginge, LoadCanvasTemplate,  validateCaptcha } from 'react-simple-captcha';
-import { AuthContext } from '../../providers/AuthProvider';
-import { Link } from 'react-router-dom';
+import React from 'react';
 
-const Login = () => {
-    const captchaRef = useRef(null);
-    const [disabled, setDisabled] = useState(true);
-
-    const {signIn} = useContext(AuthContext);
-
-    useEffect(()=>{
-        loadCaptchaEnginge(6);
-    },[])
-
-    const handleValidateCaptcha = () =>{
-        const user_captcha_value = captchaRef.current.value;
-        if(validateCaptcha(user_captcha_value)){
-            setDisabled(false);
-        }else{
-            setDisabled(true);
-        } 
-    }
-
-    const handleLogin = event => {
-        event.preventDefault();
-        const form = event.target;
-        const email = form.email.value;
-        const password = form.password.value;
-        console.log(email, password);
-        signIn(email, password)
-        .then(result => {
-            const user = result.user;
-            console.log(user);
-        })
-    }
+const Signup = () => {
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col md:flex-row">
                 <div className="text-center md:w-1/2 lg:text-left">
-                    <h1 className="text-5xl font-bold">Login now!</h1>
+                    <h1 className="text-5xl font-bold">Please Sign up</h1>
                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                 </div>
                 <div className="card  md:w-1/2 max-w-sm shadow-2xl bg-base-100">
@@ -69,11 +36,10 @@ const Login = () => {
                             <input disabled={disabled} type="submit" className="btn btn-primary" value="Login" />
                         </div>
                     </form>
-                    <p><small>New Here? <Link to={`/signup`}>Create an account</Link></small></p>
                 </div>
             </div>
         </div>
     );
 };
 
-export default Login;
+export default Signup;
