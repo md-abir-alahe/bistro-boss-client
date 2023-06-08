b7853f109cb468d57e68405d09763dfa6088efb6
md-abir-alahe <mdabiralahe@gmail.com>
2023-06-08 23:31:43 +0600

    explore react hook form validation, Error Handling

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,39 +1,48 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const Signup = () => {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = data => {
+        console.log(data)
+    };
+
     return (
         <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content flex-col md:flex-row">
-                <div className="text-center md:w-1/2 lg:text-left">
-                    <h1 className="text-5xl font-bold">Please Sign up</h1>
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Login now!</h1>
                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                 </div>
-                <div className="card  md:w-1/2 max-w-sm shadow-2xl bg-base-100">
-                    <form onSubmit={handleLogin} className="card-body">
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input {...register("name", { required: true })} type="name" name='name' placeholder="name" className="input input-bordered" />
+                            {errors.name && <span className='text-red-600'>Name is required</span>}
+                        </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="email" name='email' placeholder="email" className="input input-bordered" />
+                            <input {...register("email", {required:true})} type="email" name='email' placeholder="email" className="input input-bordered" />
+                            {errors.name && <span className='text-red-600'>Email is required</span>}
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="password" name='password' placeholder="password" className="input input-bordered" />
+                            <input  {...register("password", {required: true, minLength:6, maxLength:20})} type="password" name='password' placeholder="password" className="input input-bordered" />
+                            {errors.password?.type === 'required' && <p className='text-red-600'>Password is required</p>}
+
                             <label className="label">
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                             </label>
                         </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <LoadCanvasTemplate />
-                            </label>
-                            <input ref={captchaRef} type="text" name='captcha' placeholder="type the captcha above" className="input input-bordered" />
-                            <button onClick={handleValidateCaptcha} className='btn btn-outline btn-xs mt-2'>validate</button>
-                        </div>
                         <div className="form-control mt-6">
-                            <input disabled={disabled} type="submit" className="btn btn-primary" value="Login" />
+                            <button className="btn btn-primary">Login</button>
                         </div>
                     </form>
                 </div>
