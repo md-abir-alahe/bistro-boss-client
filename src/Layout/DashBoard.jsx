a81640c446153a080a27b496cdfd2faa8c7ccfa9
md-abir-alahe <mdabiralahe@gmail.com>
2023-06-14 03:46:21 +0600

    done

--- src/Layout/DashBoard.jsx
+++ src/Layout/DashBoard.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 import {FaShoppingCart, FaWallet, FaCalendarAlt, FaHome} from 'react-icons/fa'
 
 const DashBoard = () => {
     return (
         <div>
-            <div className="drawer md:drawer-open">
-                <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+            <div className="drawer md:drawer-open bg-[#D1A054]">
+                <input id="my-drawer-2" type="checkbox" className="drawer-toggle checked:opacity-100" />
                 <div className="drawer-content flex flex-col items-center justify-center">
                     {/* Page content here */}
                     <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button">Open drawer</label>
                     <Outlet></Outlet>
                 </div>
-                <div className="drawer-side">
+                <div className="drawer-side ">
                     <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-                    <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
+                    <ul className="menu p-4 w-80 h-full text-base-content">
                         {/* Sidebar content here */}
-                        <li><Link to={``}><FaHome></FaHome> User Home</Link></li>
-                        <li><Link to={``}><FaCalendarAlt></FaCalendarAlt> Reservations</Link></li>
-                        <li><Link to={``}><FaWallet></FaWallet> Pricing History</Link></li>
-                        <li><Link to={`/dashboard/mycart`}><FaShoppingCart></FaShoppingCart> My Cart</Link></li>
+                        <li><NavLink to={``}><FaHome></FaHome> User Home</NavLink></li>
+                        <li><NavLink to={``}><FaCalendarAlt></FaCalendarAlt> Reservations</NavLink></li>
+                        <li><NavLink to={``}><FaWallet></FaWallet> Pricing History</NavLink></li>
+                        <li><NavLink to={`/dashboard/mycart`}><FaShoppingCart></FaShoppingCart> My Cart</NavLink></li>
                         <div className='divider'></div>
-                        <li><Link to={`/`}><FaHome></FaHome> Home</Link></li>
-                        <li><Link to={`/menu`}> Our Menu</Link></li>
-                        <li><Link to={`/order/salad`}>Order</Link></li>
+                        <li><NavLink to={`/`}><FaHome></FaHome> Home</NavLink></li>
+                        <li><NavLink to={`/menu`}> Our Menu</NavLink></li>
+                        <li><NavLink to={`/order/salad`}>Order</NavLink></li>
                     </ul>
 
                 </div>
--- src/pages/DashBoard/MyCart/MyCart.jsx
+++ src/pages/DashBoard/MyCart/MyCart.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
 
 const MyCart = () => {
     return (
         <div>
+            <Helmet>
+                <title>Bistro Boss | My Cart</title>
+            </Helmet>
             <p>My cart</p>
         </div>
     );
