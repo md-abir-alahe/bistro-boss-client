95daf4d2e1f43a2b2f3f5cedf196549fce900431
md-abir-alahe <mdabiralahe@gmail.com>
2023-06-14 14:36:58 +0600

    save user if he doesn't exists in the database

--- src/pages/Shared/SocialLogin/SocialLogin.jsx
+++ src/pages/Shared/SocialLogin/SocialLogin.jsx
@@ -27,11 +27,8 @@ const SocialLogin = () => {
                 body: JSON.stringify(saveUser)
             })
                 .then(res => res.json())
-                .then(data => {
-                    if (data.insertedId) {
-                       
+                .then(() => {
                         navigate(from, { replace: true });
-                    }
                 })
 
             
