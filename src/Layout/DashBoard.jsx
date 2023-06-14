05075e713aaedeb59d982791238c2a5feb65d557
md-abir-alahe <mdabiralahe@gmail.com>
2023-06-14 19:43:41 +0600

    load all users on the dashboard page

--- src/Layout/DashBoard.jsx
+++ src/Layout/DashBoard.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
-import {FaShoppingCart, FaWallet, FaCalendarAlt, FaHome} from 'react-icons/fa'
+import { FaShoppingCart, FaWallet, FaCalendarAlt, FaHome, FaUtensils, FaUser, FaUsers } from 'react-icons/fa'
 import useCart from '../hooks/useCart';
 
 const DashBoard = () => {
     const [cart] = useCart();
+
+
+    // TODO: load data from the server to have dynamic isAdmin based on data
+    const isAdmin = true;
+
     return (
         <div>
             <div className="drawer md:drawer-open ">
@@ -17,13 +22,27 @@ const DashBoard = () => {
                 <div className="drawer-side ">
                     <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
                     <ul className="menu p-4 w-80 h-full text-base-content bg-[#D1A054]">
-                        {/* Sidebar content here */}
-                        <li><NavLink to={`/dashboard/home`}><FaHome></FaHome> User Home</NavLink></li>
-                        <li><NavLink to={`/dashboard/reservations`}><FaCalendarAlt></FaCalendarAlt> Reservations</NavLink></li>
-                        <li><NavLink to={`/dashboard/history`}><FaWallet></FaWallet> Pricing History</NavLink></li>
-                        <li>
-                            <NavLink to={`/dashboard/mycart`}><FaShoppingCart></FaShoppingCart> My Cart <div className="badge badge-secondary ms-2">+{cart?.length || 0}</div></NavLink>
-                        </li>
+                        {
+                            isAdmin ? <>
+                                <li><NavLink to={`/dashboard/home`}><FaHome></FaHome> Admin Home</NavLink></li>
+                                <li><NavLink to={`/dashboard/reservations`}><FaUtensils></FaUtensils> Add Items</NavLink></li>
+                                <li><NavLink to={`/dashboard/history`}><FaWallet></FaWallet> Manage Items</NavLink></li>
+                                <li><NavLink to={`/dashboard/history`}><FaWallet></FaWallet> Manage Bookings</NavLink></li>
+                                <li><NavLink to={`/dashboard/allusers`}><FaUsers></FaUsers> Manage Users</NavLink></li>
+                                
+                            </> : <>
+                                <li><NavLink to={`/dashboard/home`}><FaHome></FaHome> User Home</NavLink></li>
+                                <li><NavLink to={`/dashboard/reservations`}><FaCalendarAlt></FaCalendarAlt> Reservations</NavLink></li>
+                                <li><NavLink to={`/dashboard/history`}><FaWallet></FaWallet> Pricing History</NavLink></li>
+                                <li>
+                                    <NavLink to={`/dashboard/mycart`}><FaShoppingCart></FaShoppingCart> My Cart <div className="badge badge-secondary ms-2">+{cart?.length || 0}</div></NavLink>
+                                </li>
+                            </>
+                        }
+
+
+
+
                         <div className='divider'></div>
                         <li><NavLink to={`/`}><FaHome></FaHome> Home</NavLink></li>
                         <li><NavLink to={`/menu`}> Our Menu</NavLink></li>
@@ -33,7 +52,7 @@ const DashBoard = () => {
                 </div>
             </div>
         </div>
-        
+
     );
 };
 
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -11,6 +11,7 @@ import PrivateRoute from "./PrivateRoute";
 import Secret from "../pages/Shared/Secret/Secret";
 import DashBoard from "../Layout/DashBoard";
 import MyCart from "../pages/DashBoard/MyCart/MyCart";
+import AllUsers from "../pages/DashBoard/AlllUsers/AllUsers";
 
 const router = createBrowserRouter([
     {
@@ -50,6 +51,10 @@ const router = createBrowserRouter([
             {
                 path: 'mycart',
                 element: <MyCart></MyCart>
+            },
+            {
+                path: 'allusers',
+                element: <AllUsers></AllUsers>
             }
         ]
     }
