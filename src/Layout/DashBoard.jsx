99e24aba73f4820f8b78141430c35344bd8d85aa
md-abir-alahe <mdabiralahe@gmail.com>
2023-06-14 12:06:52 +0600

    display cart items to review before order

--- src/Layout/DashBoard.jsx
+++ src/Layout/DashBoard.jsx
@@ -7,7 +7,7 @@ const DashBoard = () => {
         <div>
             <div className="drawer md:drawer-open ">
                 <input id="my-drawer-2" type="checkbox" className="drawer-toggle checked:opacity-100" />
-                <div className="drawer-content flex flex-col items-center justify-center">
+                <div className="drawer-content flex flex-col items-center">
                     {/* Page content here */}
                     <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button">Open drawer</label>
                     <Outlet></Outlet>
